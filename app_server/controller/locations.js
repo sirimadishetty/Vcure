a51bd4cbc325f5a11ba050de2c99fca02bbe0c04
sirimadishetty <21eg107a29@anurag.edu.in>
2023-08-09 12:12:10 +0530

    First commit

---
+++ app_server/controller/locations.js
@@ -0,0 +1,15 @@
+module.exports.homeList= function(req,res){
+    res.render('location-list',{title:'Home'});
+};
+module.exports.locationInfo= function(req,res){
+    res.render('location-info',{title:'location '});
+};
+module.exports.locationInfo1= function(req,res){
+    res.render('location-info1',{title:'review'});
+};
+module.exports.locationInfo2= function(req,res){
+    res.render('location-info2',{title:'review'});
+};
+module.exports.addReview= function(req,res){
+    res.render('location-review-form',{title:'review'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain=require('../controller/main');
-/*GET home page */
-router.get('/',ctrlMain.index);
-
+const ctrlLocations=require('../controller/locations');
+const ctrlOthers=require('../controller/others');
+/* GET home page. */
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location1', ctrlLocations.locationInfo1);
+router.get('/location2', ctrlLocations.locationInfo2);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/about',ctrlOthers.about);
+router.get('/signin',ctrlOthers.signin);
+router.get('/register',ctrlOthers.register);
 module.exports = router;
